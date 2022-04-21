2e9ce05e3f2a707b07176961c10cc5f553ca42c2
Imavin08 <92112933+Imavin08@users.noreply.github.com>
2022-04-21 22:55:00 +0200

    add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const refs = {};
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.start.addEventListener('click', startChangeColor);
+refs.stop.addEventListener('click', stopChangeColor);
+
+let interval = false;
+let timer = null;
+
+function startChangeColor() {
+  if (interval) {
+    return;
+  } else {
+    timer = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    interval = true;
+  }
+}
+
+function stopChangeColor() {
+  interval = false;
+  clearInterval(timer);
+}
